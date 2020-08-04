c089e5b0b4a3fa33d5a8772216c7833a69e9d7b1
Lucas Soares <lsoareshfb@gmail.com>
2020-08-04 17:38:14 -0300

    Validação de erros, faltando apenas mostra-los no front

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,12 +1,18 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerProps {
+    isFocused: boolean;
+    isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
     width: 100%;
-    border: 1px solid #232129;
     border-radius: 10px;
     background: #232129;
     padding: 18px;
+
     color: #666360;
+    border: 2px solid #232129;
 
     display: flex;
     align-items: center;
@@ -14,6 +20,15 @@ export const Container = styled.div`
     & + div {
         margin-top: 8px;
     }
+
+    ${props => props.isFocused && css`
+        color: #ff9000;
+        border: 2px solid #ff9000;
+    `};
+
+    ${props => props.isFilled && css`
+        color: #ff9000;
+    `};
 
     input {
         flex: 1;
